21572b901e3864b6ac08d40c3bfd0f6eb5005b5d
sergiodrm <sergiojg.drm@gmail.com>
2021-11-29 01:40:58 +0100

    Added UICanvas and ItemGrid script file

---
+++ GhostMansion/Assets/Source/ItemGrid.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrid : MonoBehaviour
+{
+    private List<Item> Items;
+    private ItemManager ItemManager;
+    
+
+    public int Width;
+    public int Height;
+
+    private void Start()
+    {
+        
+    }
+}
